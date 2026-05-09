054303fa50adae96e49545283987571648560982
Julio Ortiz <ingeniero.julioortiz@gmail.com>
2026-05-08 18:40:10 -0600

    Cambios: nuevos códigos para semana3

--- Semana3/MayorMenor.java
+++ Semana3/MayorMenor.java
@@ -1,20 +1,22 @@
 package Semana3;
+
 import java.util.Scanner;
 
 public class MayorMenor {
-    static int numero1,numero2=0;
-    public static void main (String[] args){
-        Scanner entrada=new Scanner (System.in);
+    static int numero1, numero2 = 0;
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
         System.out.println("Digite el primer numero: ");
-        numero1=entrada.nextInt();
+        numero1 = entrada.nextInt();
         System.out.println("Digite el segundo numero: ");
-        numero2=entrada.nextInt();
-    String resultado=(numero1>numero2)?"El numero mayor es: " + numero1:"El numero mayor es: " + numero2;
-    System.out.println(resultado);
-    if(numero1>numero2)
-        System.out.println("El numero mayor es: " + numero1);
-      else
-        System.out.println("El numero mayor es: " + numero2);
-    entrada.close();
-}
+        numero2 = entrada.nextInt();
+        String resultado = (numero1 > numero2) ? "El numero mayor es: " + numero1 : "El numero mayor es: " + numero2;
+        System.out.println(resultado);
+        if (numero1 > numero2)
+            System.out.println("El numero mayor es: " + numero1);
+        else
+            System.out.println("El numero mayor es: " + numero2);
+        entrada.close();
+    }
 }
