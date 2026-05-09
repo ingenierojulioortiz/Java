9d7f62d8d1b6000695647e9ec23b165452154cff
Julio Ortiz <ingeniero.julioortiz@gmail.com>
2026-05-08 18:58:33 -0600

    Archivo corregido @Perritos.java

--- Perritos.java
+++ Perritos.java
@@ -4,17 +4,17 @@ public class Perritos {
     private String nombre;
     private int edad;
 
-    private Perritos(String raza, String nombre, int edad) {
+    public Perritos(String raza, String nombre, int edad) {
         this.raza= raza;
         this.nombre= nombre;
         this.edad= edad;
+    }
 
     public String toString () {
             return "Perritos{" +
             "raza=" + raza + "\"" + 
             "nombre=" + nombre + "\"" +
-            "edad," + edad + 
-            ´}´:
-}}
-
+            "edad=" + edad + 
+            "}";
+    }
 }
