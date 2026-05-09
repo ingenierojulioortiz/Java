14d1d819c5bb306d6a71523b5d08592fde168aa5
Julio Ortiz <ingeniero.julioortiz@gmail.com>
2026-05-08 18:47:21 -0600

    Cambios: Modificaciones al @AdivinaNumero.java

--- AdivinaNumero.java
+++ AdivinaNumero.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class AdivinaNumero {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int secreto = 42;  // número a adivinar
+        int secreto = (int) (Math.random() * 100) + 1; // número a adivinar
         int intento = 0;
         int intentos = 0;
         System.out.println("=== ADIVINA EL NÚMERO (entre 1 y 100) ===");
@@ -11,14 +11,12 @@ public class AdivinaNumero {
             System.out.print("Tu intento: ");
             intento = sc.nextInt();
             intentos++;
-
             if (intento < secreto) {
                 System.out.println("Demasiado bajo. Intenta más alto.");
             } else if (intento > secreto) {
                 System.out.println("Demasiado alto. Intenta más bajo.");
             }
         }
-
         System.out.println("¡Correcto! Lo lograste en " + intentos + " intento(s).");
         sc.close();
     }
