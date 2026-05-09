6a220a99babf263ffe23d0f7a807f8ae436b79b2
Julio Ortiz <ingeniero.julioortiz@gmail.com>
2026-05-08 18:56:38 -0600

    Nuevo código java @Perritos.java

---
+++ Perritos.java
@@ -0,0 +1,20 @@
+public class Perritos {
+
+    private String raza;
+    private String nombre;
+    private int edad;
+
+    private Perritos(String raza, String nombre, int edad) {
+        this.raza= raza;
+        this.nombre= nombre;
+        this.edad= edad;
+
+    public String toString () {
+            return "Perritos{" +
+            "raza=" + raza + "\"" + 
+            "nombre=" + nombre + "\"" +
+            "edad," + edad + 
+            ´}´:
+}}
+
+}
